dcc3cc0ba6a98e7975fbf6ccbc849c535e6393b6
martix47 <marta.andrzejczak47@gmail.com>
2025-01-06 17:03:10 +0000

    logowanie

---
+++ projekt.js
@@ -0,0 +1,20 @@
+const validUsername = "admin";
+const validPassword = "password123";
+
+const loginForm = document.getElementById('loginForm');
+const errorMessage = document.getElementById('errorMessage');
+
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Zapobiega przeładowaniu strony
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === validUsername && password === validPassword) {
+        // Jeśli dane są poprawne, przekierowanie na stronę główną
+        window.location.href = "index.html";
+    } else {
+        // Wyświetl błąd
+        errorMessage.style.display = "block";
+    }
+});
