0cd1545fe2447e4577f6d967566d7c6be04de3e5
martix47 <marta.andrzejczak47@gmail.com>
2025-01-10 11:54:15 +0000

    zmiana logiki logowania

--- login.js
+++ login.js
@@ -6,24 +6,28 @@ const loginForm = document.getElementById('loginForm');
 const errorMessage = document.getElementById('errorMessage');
 
 loginForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Zapobiega przeładowaniu strony
+    event.preventDefault(); //  przeładowanie
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
     if (username === validUsername && password === validPassword) {
-        // Jeśli dane są poprawne, przekierowanie na stronę główną
-        window.location.href = "index.html";
-    } else {
-        // Wyświetl błąd
-        errorMessage.style.display = "block";
-    }
-    // Jeśli zaznaczono "Zapamiętaj mnie", zapisz dane do localStorage
+        
+        localStorage.setItem('loggedIn', true);
+    
+    //Zapamiętaj mnie
     const rememberMe = document.getElementById('rememberMe');
 
     if (rememberMe.checked) {
         localStorage.setItem('username', username);
     } else {
         localStorage.removeItem('username');
-    }
+    }     
+
+    
+    window.location.href = "index.html";
+    } else {
+    
+    errorMessage.style.display = "block";
+}
 });
